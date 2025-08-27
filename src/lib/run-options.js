d436563f546cde4956f67d8444bc100bad8b1f44
pclaesen <pieterclaesen@gmail.com>
2025-08-27 20:14:39 +0200

    Feat.: Add hardcoded distances

---
+++ src/lib/run-options.js
@@ -0,0 +1,23 @@
+export const RUN_DISTANCE_OPTIONS = [
+  {
+    value: '10m',
+    label: '10m run',
+    description: '10 meter sprint'
+  },
+  {
+    value: '100m',
+    label: '100m run', 
+    description: '100 meter dash'
+  },
+  {
+    value: '1km',
+    label: '1km run',
+    description: '1 kilometer run'
+  }
+];
+
+export const DEFAULT_RUN_OPTION = {
+  value: '',
+  label: 'Select run distance...',
+  description: 'Choose a run distance to sponsor'
+};
